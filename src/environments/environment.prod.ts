6ed38f7701b1bcd7356a2e2bfa135aad152b155b
sopelan <marcossopelana@gmail.com>
2021-10-01 18:35:46 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA6Brlzb3sbCGuccmBNsQfud-H9A848jgc",
+    authDomain: "simulacroparcial-3dbe5.firebaseapp.com",
+    projectId: "simulacroparcial-3dbe5",
+    storageBucket: "simulacroparcial-3dbe5.appspot.com",
+    messagingSenderId: "866467128658",
+    appId: "1:866467128658:web:0a21ae8a724adbf804c68b",
+    measurementId: "G-NZJ8XLGY1Q"
+  }
 };
